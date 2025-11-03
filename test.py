98609e1a25463d381d3e3fcd0d638adbcff08565
Annhilati <antbe.artis@web.de>
2025-11-03 15:26:16 +0100

    Sync

---
+++ test.py
@@ -0,0 +1,7 @@
+from beet import Context, JsonFileBase
+from typing import Any
+
+# def beet_defaul(ctx: Context):
+#     for i, t in enumerate(ctx.data.extend_namespace):
+#         if t is JsonFileBase:
+#             ctx.data.extend_namespace[i] =
