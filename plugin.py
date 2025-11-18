f46f058658a72ec7e62a9754f7acae9ffe4d9969
Annhilati <antbe.artis@web.de>
2025-11-18 11:01:51 +0100

    Sync

--- plugin.py
+++ plugin.py
@@ -1,5 +1,13 @@
-from beet import Context, JsonFileBase
+from beet import Context, JsonFileBase, JsonFile
+from typing import Any
 from json5 import loads
 
-def beet_default(ctx: Context):
+def require(ctx: Context):
     JsonFileBase.decoder = loads
+
+def pipeline(ctx: Context):
+    JsonFileBase.decoder = loads
+    for _, json_file in ctx.data.list_files(extend=JsonFileBase[Any]):
+        print(_)
+        print(json_file.data)
+        json_file.text = json_file.encoder(json_file.data)
