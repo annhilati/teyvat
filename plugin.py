4b43f621f2b65086c00e13a8b23b7e95d8e8d99c
Annhilati <antbe.artis@web.de>
2026-02-20 14:45:46 +0100

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -6,23 +6,24 @@ import Rhombus
 def beet_default(ctx: Context):
     data = ctx.data
 
-    # Fill surface_rule with external yaml file
-    with open("./pack/data/teyvat/worldgen/noise_settings/surface_rules.yml", "r", encoding="utf-8") as f:
-        data[WorldgenNoiseSettings]["teyvat:teyvat"].data["surface_rule"] = yaml.load(f, Loader=yaml.SafeLoader)
+    # # Fill surface_rule with external yaml file
+    # with open("./pack/data/teyvat/worldgen/noise_settings/surface_rules.yml", "r", encoding="utf-8") as f:
+    #     data[WorldgenNoiseSettings]["teyvat:teyvat"].data["surface_rule"] = yaml.load(f, Loader=yaml.SafeLoader)
     
-    # Fill biomes with external yaml file
-    with open("./pack/data/teyvat/dimension/biomes.yml", "r", encoding="utf-8") as f:
-        data[Dimension]["teyvat:teyvat"].data["generator"]["biome_source"]["biomes"] = yaml.load(f, Loader=yaml.SafeLoader)
+    # # Fill biomes with external yaml file
+    # with open("./pack/data/teyvat/dimension/biomes.yml", "r", encoding="utf-8") as f:
+    #     data[Dimension]["teyvat:teyvat"].data["generator"]["biome_source"]["biomes"] = yaml.load(f, Loader=yaml.SafeLoader)
 
-    from terrain.natlan import OUT as natlan_terrain
-    from terrain.plane_of_euthymia import FINAL_DESTINY as plane_of_euthymia_terrain
-    from terrain.primordial_sea import FINAL_DESTINY as primordial_sea_terrain
-    natlan_terrain.inject(ctx.data, "teyvat:natlan_terrain")
-    plane_of_euthymia_terrain.inject(ctx.data, "teyvat:plane_of_euthymia_final_destiny")
-    primordial_sea_terrain.inject(ctx.data, "teyvat:primordial_sea_final_destiny")
+    # from terrain.natlan import OUT as natlan_terrain
+    # from terrain.plane_of_euthymia import FINAL_DESTINY as plane_of_euthymia_terrain
+    # from terrain.primordial_sea import FINAL_DESTINY as primordial_sea_terrain
+    # natlan_terrain.inject(ctx.data, "teyvat:natlan_terrain")
+    # plane_of_euthymia_terrain.inject(ctx.data, "teyvat:plane_of_euthymia_final_destiny")
+    # primordial_sea_terrain.inject(ctx.data, "teyvat:primordial_sea_final_destiny")
     
-    t = Rhombus.coords.x()
-    print(t)
-    t.inject(ctx.data, "test")
+    other = Rhombus.constant(5)
+    d = Rhombus.constant(1) + Rhombus.ExternalDensity(other)
+    print(d)
+    d.inject(data, "a:function", log=False)
 
-    print(Rhombus.Density.from_datapack(ctx.data, "test"))
+    print(Rhombus.Density.from_datapack(data, "a:function"))
